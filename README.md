c96d26d7dc842aae59442b4ff2e96c72fd7e9233
g4br13lr0ch4 <gabriel.gabrielroch@gmail.com>
2020-09-18 10:31:39 -0300

    inicio da implementação do -turma entrar-, separação do token

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Criado com o objetivo de gerenciar multiplas turmas dentro de um servidor.
   Funções:
-    $addTurma - Cria um cargo com o nome da turma.(Preferencialmente nome da turma+professor. Ex..: Estrutura_de_dados-Joao)
-    $enterTurma - Permite um usuario entrar em uma turma( ou cargo)
-    $exitTurma - Permite que o aluno saia de uma turma (ou cargo)
+    $turma add- Cria um cargo com o nome da turma.(Preferencialmente nome da turma+professor. Ex..: Estrutura_de_dados-Joao)
+    $turma entrar- Permite um usuario entrar em uma turma( ou cargo)
+    $turma sair- Permite que o aluno saia de uma turma (ou cargo)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import discord
+import asyncio
+from gtoken import retornatoken
 
 comando = ["$turma entrar", "$turma sair", "$turma entrar", "$infos"]
 
@@ -6,32 +8,41 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Rodando {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$Turma'):
+    if message.content.startswith('$turma add'):
         await message.channel.send('Esse comando permite que você adicione uma turma/cargo!')
     
     elif message.content.startswith('$exitTurma'):
         await message.channel.send('Esse comando permite que você  saia de uma turma/cargo que você está incluso!')
     
     elif message.content.startswith('$turma entrar'):
-        await message.channel.send('Esse comando permite que você entre em uma turma/cargo existente!')
+        embed1 = discord.Embed(
+                    title="Escolha a turma que deseja entrar!",
+                    color=0x690FC3,
+                    description="- IHC = 🐤\n"
+                        "- Grafos  =  📘 \n"
+                        "- LIP-Rodrigo  = 📙",)
+
+        botmsg = await message.channel.send(embed=embed1)
+        await message   .add_reaction(botmsg, "🐤")
+        await message.add_reaction("📘")
+        await message.add_reaction("📙")
 
     elif message.content.startswith('$info'):
         await message.channel.send('O BODOC tem como objetivo reunir alunos das mesmas turmas ' +
         'facilitando assim a comunicação entre eles.')
-        mensagem = "Esses são todos os comandos disponiveis:"
+        mensagem = "\nEsses são todos os comandos disponiveis:"
         for item in comando:
             if item == "$infos":
                 mensagem += "\n" + item
             else:
                 mensagem += "\n" + item + " nome_da_turma"
         await message.channel.send(mensagem)
-
         
-client.run('NzU2MjU5MDY2MTExNTkwNDMz.X2PPIw.y0J-AXu864Ro5gNmKmCiUxCYDv0')
+client.run(retornatoken())
